e2b71f777c091173e5e7e495bdde066d3d7b63fd
2394650 <a_rahul.gupta@africa.airtel.com>
2020-07-07 22:56:38 +0530

    Add POM Page For Supervisor Login

---
+++ src/main/java/pages/agentLoginPagePOM.java
@@ -0,0 +1,42 @@
+package pages;
+
+import lombok.extern.log4j.Log4j2;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+@Log4j2
+public class agentLoginPagePOM extends BasePage {
+
+    By loginQueueTitle = By.xpath("//h2[contains(text(),'Login Queue')]");
+    By selectQueue = By.xpath("//span[contains(text(),'Select Sub Workgroup (Queue)')]");
+    By skipBtn = By.xpath("//span[contains(text(),'Skip > >')]");
+    By submitBtn = By.className("btn btn-save");
+    public agentLoginPagePOM(WebDriver driver) {
+        super(driver);
+    }
+
+    public boolean isQueueLoginPage(){
+        log.info("Supervisor Login Page");
+        return checkState(loginQueueTitle);
+    }
+
+    public void clickSelectQueue(){
+        log.info("Select Login Queue");
+        click(selectQueue);
+    }
+
+    public void clickSkipBtn(){
+        log.info("Clicking on SKIP Button");
+        click(skipBtn);
+    }
+
+    public void clickSubmitBtn(){
+        log.info("Clicking on Submit Button");
+        click(submitBtn);
+    }
+
+    public void byQueueName(String queueName) throws InterruptedException {
+        log.info("Selecting Queue : "+queueName);
+        selectByText(queueName);
+    }
+}
