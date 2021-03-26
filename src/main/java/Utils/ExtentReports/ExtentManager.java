c5da223ecfece688581fab1d57be79cfabae1781
Rahul Gupta <a_rahul.gupta@africa.airtel.com>
2021-03-26 12:10:24 +0530

    Report Name Update

--- src/main/java/Utils/ExtentReports/ExtentManager.java
+++ src/main/java/Utils/ExtentReports/ExtentManager.java
@@ -27,7 +27,7 @@ public class ExtentManager {
                 extent.loadConfig(new File(workingDir + "\\src\\main\\resources\\reportextent-config.xml"));
 
             } else {
-                extent = new ExtentReports(workingDir + "/ExtentReports/CS_Portal-Automation-Report-" + BaseTest.Opco + BaseTest.Env + "-" + BaseTest.suiteType + "-" + date + ".html", true);
+                extent = new ExtentReports(workingDir + "/ExtentReports/CS_Portal-Automation-Report-" + BaseTest.Opco + BaseTest.Env + "-" + date + ".html", true);
                 extent.addSystemInfo("user", "Rahul Gupta");
                 extent.assignProject("CS Portal");
                 extent.addSystemInfo("OPCO", BaseTest.Opco);
