eb7cb6e1e5e9e787ef4a5b022dcd03ba574c96f7
a13221514 <ashwani.ashwani@africa.airtel.com>
2021-04-28 18:21:54 +0530

    Automation for CS
    Code optimisation and assertion method changed.

--- src/test/java/com/airtel/cs/ui/frontendagent/SystemStatusTest.java
+++ src/test/java/com/airtel/cs/ui/frontendagent/SystemStatusTest.java
@@ -1,5 +1,6 @@
 package com.airtel.cs.ui.frontendagent;
 
+import com.airtel.cs.common.actions.BaseActions;
 import com.airtel.cs.commonutils.PassUtils;
 import com.airtel.cs.commonutils.dataproviders.DataProviders;
 import com.airtel.cs.commonutils.dataproviders.TestDatabean;
@@ -10,17 +11,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.http.Header;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
 
 import java.util.concurrent.TimeUnit;
 
 public class SystemStatusTest extends Driver {
 
+    private final BaseActions actions = new BaseActions();
+
+
     @DataProviders.User(userType = "API")
     @Test(dataProvider = "loginData", dataProviderClass = DataProviders.class, priority = 1)
     public void loginAPI(TestDatabean data) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
-        SoftAssert softAssert = new SoftAssert();
         final String loginAUUID = data.getLoginAUUID();
         LoginPOJO Req = LoginPOJO.loginBody(loginAUUID, PassUtils.decodePassword(data.getPassword()));
         selUtils.addTestcaseDescription("Logging in Using Login com.airtel.cs.API for getting TOKEN with user : " + loginAUUID, "description");
@@ -34,28 +36,19 @@ public class SystemStatusTest extends Driver {
             map.add(new Header("Authorization", token));
             commonLib.info("Response Body : " + response.asString());
             commonLib.info("Response time : " + response.getTimeIn(TimeUnit.SECONDS) + " s");
-            if (response.jsonPath().getString("message").equalsIgnoreCase("Failed to authenticate user.")) {
-                continueExecutionAPI = false;
-                softAssert.fail("Not able to generate Token. Please Update Password As soon as possible if required.\nAPI Response Message: " + response.jsonPath().getString("message"));
-            } else if (response.jsonPath().getString("message").toLowerCase().contains("something went wrong")) {
-                continueExecutionAPI = false;
-                softAssert.fail("Not able to generate Token. Login API Failed(Marked Build As Failed).\ncom.airtel.cs.API Response Message: " + response.jsonPath().getString("message"));
-            } else if (!response.jsonPath().getString("message").equalsIgnoreCase("User authenticated successfully")) {
-                continueExecutionAPI = false;
-                softAssert.fail("Not able to generate Token. Please Check the API error Message and make changes if required.\ncom.airtel.cs.API Response Message: " + response.jsonPath().getString("message"));
-            }
+            final String message = response.jsonPath().getString("message");
+            assertCheck.append(actions.assertEqual_stringType(message, "User authenticated successfully", "User authenticated successfully", message));
         } catch (Exception e) {
             continueExecutionAPI = false;
-            softAssert.fail("Connectivity issue occurred, Not able to connect with server : " + e.fillInStackTrace());
+            commonLib.fail("Connectivity issue occurred, Not able to connect with server : " + e.fillInStackTrace(), true);
         }
-        softAssert.assertAll();
+        actions.assertAllFoundFailedAssert(assertCheck);
     }
 
     @DataProviders.User()
     @Test(dataProvider = "loginData", dataProviderClass = DataProviders.class, priority = 2)
     public void testCredOfAdmin(TestDatabean data) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
-        SoftAssert softAssert = new SoftAssert();
         final String loginAUUID = data.getLoginAUUID();
         LoginPOJO Req = LoginPOJO.loginBody(loginAUUID, PassUtils.decodePassword(data.getPassword()));
         String dtoAsString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(Req);
@@ -65,32 +58,20 @@ public class SystemStatusTest extends Driver {
             final Response response = pages.getLoginPage().loginAPI(dtoAsString);
             commonLib.info("Response Body : " + response.asString());
             commonLib.info("Response time : " + response.getTimeIn(TimeUnit.SECONDS) + " s");
-            if (response.jsonPath().getString("message").equalsIgnoreCase("Failed to authenticate user.")) {
-                continueExecutionFA = false;
-                continueExecutionBS = false;
-                softAssert.fail("Not able to generate Token. Please Update Password As soon as possible if required.\ncom.airtel.cs.API Response Message: " + response.jsonPath().getString("message"));
-            } else if (response.jsonPath().getString("message").toLowerCase().contains("something went wrong")) {
-                continueExecutionFA = false;
-                continueExecutionBS = false;
-                softAssert.fail("Not able to generate Token. Login com.airtel.cs.API Failed(Marked Build As Failed).\ncom.airtel.cs.API Response Message: " + response.jsonPath().getString("message"));
-            } else if (!response.jsonPath().getString("message").equalsIgnoreCase("User authenticated successfully")) {
-                continueExecutionFA = false;
-                continueExecutionBS = false;
-                softAssert.fail("Not able to generate Token. Please Check the com.airtel.cs.API error Message and make changes if required.\ncom.airtel.cs.API Response Message: " + response.jsonPath().getString("message"));
-            }
+            final String message = response.jsonPath().getString("message");
+            assertCheck.append(actions.assertEqual_stringType(message,"User authenticated successfully","User authenticated successfully", message));
         } catch (Exception e) {
             continueExecutionFA = false;
             continueExecutionBS = false;
-            softAssert.fail("Connectivity issue occurred, Not able to connect with server : " + e.fillInStackTrace());
+            commonLib.fail("Connectivity issue occurred, Not able to connect with server : " + e.fillInStackTrace(), true);
         }
-        softAssert.assertAll();
+        actions.assertAllFoundFailedAssert(assertCheck);
     }
 
     @DataProviders.User(userType = "BA")
     @Test(dataProvider = "loginData", dataProviderClass = DataProviders.class, priority = 3)
     public void testCredOfBackendAgent(TestDatabean data) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
-        SoftAssert softAssert = new SoftAssert();
         final String loginAUUID = data.getLoginAUUID();
         LoginPOJO Req = LoginPOJO.loginBody(loginAUUID, PassUtils.decodePassword(data.getPassword()));
         String dtoAsString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(Req);
@@ -100,20 +81,12 @@ public class SystemStatusTest extends Driver {
             final Response response = pages.getLoginPage().loginAPI(dtoAsString);
             commonLib.info("Response Body : " + response.asString());
             commonLib.info("Response time : " + response.getTimeIn(TimeUnit.SECONDS) + " s");
-            if (response.jsonPath().getString("message").equalsIgnoreCase("Failed to authenticate user.")) {
-                continueExecutionBA = false;
-                softAssert.fail("Not able to generate Token. Please Update Password As soon as possible if required.\ncom.airtel.cs.API Response Message: " + response.jsonPath().getString("message"));
-            } else if (response.jsonPath().getString("message").toLowerCase().contains("something went wrong")) {
-                continueExecutionBA = false;
-                softAssert.fail("Not able to generate Token. Login com.airtel.cs.API Failed(Marked Build As Failed).\ncom.airtel.cs.API Response Message: " + response.jsonPath().getString("message"));
-            } else if (!response.jsonPath().getString("message").equalsIgnoreCase("User authenticated successfully")) {
-                continueExecutionBA = false;
-                softAssert.fail("Not able to generate Token. Please Check the com.airtel.cs.API error Message and make changes if required.\ncom.airtel.cs.API Response Message: " + response.jsonPath().getString("message"));
-            }
+            final String message = response.jsonPath().getString("message");
+            assertCheck.append(actions.assertEqual_stringType(message,"User authenticated successfully","User authenticated successfully", message));
         } catch (Exception e) {
             continueExecutionBA = false;
-            softAssert.fail("Connectivity issue occurred, Not able to connect with server : " + e.fillInStackTrace());
+            commonLib.fail("Connectivity issue occurred, Not able to connect with server : " + e.fillInStackTrace(), true);
         }
-        softAssert.assertAll();
+        actions.assertAllFoundFailedAssert(assertCheck);
     }
 }
