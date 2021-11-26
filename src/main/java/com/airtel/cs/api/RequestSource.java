00a02a48762491b409e93e1d4e673a3f36b246d5
Shubham Gupta <shubham2.gupta@africa.airtel.com>
2021-11-26 18:14:00 +0530

    test cases added for autofill api

--- src/main/java/com/airtel/cs/api/RequestSource.java
+++ src/main/java/com/airtel/cs/api/RequestSource.java
@@ -168,6 +168,9 @@ public class RequestSource extends RestCommonUtils {
     private static final String MSISDN = "msisdn";
     private static final String MSISDN_CAPS = "MSISDN";
     private static final String TYPE = "type";
+    private static final String LAYOUT_CONFIG_TYPE = "layoutConfigType";
+    private static final String CATEGORY_ID = "categoryId";
+    private static final String INPUT_FIELDS = "inputFields";
     private static final String NUMBER = "number";
     private static final String ACCOUNT_NO = "accountNo";
     private static final String ACCOUNT_ID = "accountId";
@@ -296,6 +299,31 @@ public class RequestSource extends RestCommonUtils {
         } catch (Exception e) {
             commonLib.fail(constants.getValue(CS_PORTAL_API_ERROR) + " - searchAPITest " + e.getMessage(), false);
             esbRequestSource.callServiceClassRatePlan(new GenericRequest(msisdn));
+        }
+        return myList;
+    }
+
+    /**
+     * Auto fill api test list.
+     *
+     * @param layoutConfigType the layout config type
+     * @param categoryId the category id
+     * @param inputFields the input fields
+     * @return the list
+     */
+    public List<String> autoFillAPITest(String layoutConfigType, String categoryId, String inputFields) {
+        String result;
+        List<String> myList = null;
+        try {
+            queryParam.put(LAYOUT_CONFIG_TYPE, layoutConfigType);
+            queryParam.put(CATEGORY_ID, categoryId);
+            queryParam.put(INPUT_FIELDS, inputFields);
+            baseUrl = srBaseUrl;
+            commonGetMethodWithQueryParam(URIConstants.AUTOFILL_ISSUE_FIELD, queryParam);
+            result = response.print();
+            myList = new ArrayList<>(Arrays.asList(result.split("data:")));
+        } catch (Exception e) {
+            commonLib.fail(constants.getValue(CS_PORTAL_API_ERROR) + " - autoFillAPITest " + e.getMessage(), false);
         }
         return myList;
     }
