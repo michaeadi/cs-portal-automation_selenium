6b06be1dabac74766f32371c41e59314ba7ce240
a13401405 <Lakshya@#9340>
2022-01-31 12:53:59 +0530

    Prod.xml bug fixes

--- src/main/java/com/airtel/cs/model/response/postpaid/enterprise/AccountStatementCSResponse.java
+++ src/main/java/com/airtel/cs/model/response/postpaid/enterprise/AccountStatementCSResponse.java
@@ -3,7 +3,6 @@ package com.airtel.cs.model.response.postpaid.enterprise;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
-import java.util.List;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
--- src/main/java/com/airtel/cs/model/response/postpaid/enterprise/Result.java
+++ src/main/java/com/airtel/cs/model/response/postpaid/enterprise/Result.java
@@ -10,5 +10,5 @@ import java.util.List;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Result {
-    public List<MsisdnDetail> msisdnDetailList =null;
+    public List<MsisdnDetail> msisdnDetailList = null;
 }
