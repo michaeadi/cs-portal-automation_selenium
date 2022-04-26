2197471ea92405abca4cea0c2577106aa9fb1cfc
a13401545 <Michael.Adinebo@ng.airtel.com>
2022-04-26 12:10:46 +0100

    Comments

--- src/main/java/com/airtel/cs/pagerepository/pagemethods/PsbDemographicWidget.java
+++ src/main/java/com/airtel/cs/pagerepository/pagemethods/PsbDemographicWidget.java
@@ -676,6 +676,11 @@ public class PsbDemographicWidget extends BasePage{
         return text;
     }
 
+    /**
+     * This method is used to hover on Customer Id Info Icon
+     * @return
+     */
+
     public void hoverOnCustomerInfoIdIcon() {
         commonLib.info("Hover on Customer Id Info icon");
         hoverOverElement(pageElements.customerIdInfoIcon);
