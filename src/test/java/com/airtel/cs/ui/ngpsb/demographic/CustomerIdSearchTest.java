aae19c702691d742c3a3aef520f5b8d2cf199e02
a13401405 <Lakshya@#9340>
2022-05-02 18:51:32 +0530

    Automation CS Portal
    1. issue fixes

---
+++ src/test/java/com/airtel/cs/ui/ngpsb/demographic/CustomerIdSearchTest.java
@@ -0,0 +1,49 @@
+package com.airtel.cs.ui.ngpsb.demographic;
+import com.airtel.cs.api.PsbRequestSource;
+import com.airtel.cs.commonutils.applicationutils.constants.ApplicationConstants;
+import com.airtel.cs.driver.Driver;
+import com.airtel.cs.model.cs.response.psb.cs.clmdetails.CLMDetailsResponse;
+import org.testng.SkipException;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class CustomerIdSearchTest extends Driver{
+    private static String customerId = null;
+    PsbRequestSource api = new PsbRequestSource();
+    CLMDetailsResponse clmDetails;
+    String className = this.getClass().getName();
+
+
+    @BeforeMethod(groups = {"SanityTest", "RegressionTest", "ProdTest", "SmokeTest"})
+    public void checkExecution() {
+        if (!continueExecutionFA) {
+            commonLib.skip("Skipping tests because user NOT able to login Over Portal");
+            throw new SkipException("Skipping tests because user NOT able to login Over Portal");
+        }
+    }
+
+    @Test(priority = 1, groups = {"SanityTest", "RegressionTest", "ProdTest", "SmokeTest"})
+    public void openCustomerInteraction() {
+        try {
+            selUtils.addTestcaseDescription("Open Customer Profile Page with valid Customer id, Validate Customer Profile Page Loaded or not", "description");
+            customerId = constants.getValue(ApplicationConstants.CUSTOMER_ID);
+            pages.getSideMenuPage().clickOnSideMenu();
+            pages.getSideMenuPage().openCustomerInteractionPage();
+            pages.getMsisdnSearchPage().enterNumber(customerId);
+            pages.getMsisdnSearchPage().clickOnSearch();
+            clmDetails = api.getCLMDetails(customerId);
+            assertCheck.append(actions.assertEqualIntType(clmDetails.getStatusCode(), 200, "CLM Details API Status Code Matched and is :" + clmDetails.getStatusCode(), "CLM Details API Status Code NOT Matched and is :" + clmDetails.getStatusCode(), false));
+            if (clmDetails.getStatusCode() == 200) {
+                boolean pageLoaded = pages.getPsbDemographicWidget().isPageLoaded(clmDetails, className);
+                if (!pageLoaded)
+                    continueExecutionFA = false;
+            } else
+                commonLib.warning("Clm Details API is not working");
+            actions.assertAllFoundFailedAssert(assertCheck);
+        } catch (Exception e) {
+            commonLib.fail("Exception in Method - openCustomerInteraction" + e.fillInStackTrace(), true);
+        }
+    }
+
+}
